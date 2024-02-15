819445e6adb634285f6d619a7407c536e3024331
Zhang Yao <cineazhan@gmail.com>
2024-02-15 13:32:19 +0800

    初始化仓库

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,9 @@
 //! This crate is used for hold this name before the development is finished.
+
+// We still use the name string_utils which is used by others.
+#[crate_name = "string-utils"]
+
+mod string_utils;
+
+pub use string_utils::StringUtilsExt;
+
